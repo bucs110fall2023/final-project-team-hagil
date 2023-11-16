fda7e69e825bc82460c7e67c3b1477a2dd957007
Harry Do <qdo@binghamton.edu>
2023-11-15 21:47:20 -0500

    Add some keys pics and overlap

--- src/controller.py
+++ src/controller.py
@@ -10,8 +10,9 @@ RUNNING=True
 screen.fill("white")
 pygame.display.flip()
 pygame.time.wait(1000)
+w_key=pygame.image.load("final-project-team-hagil/assets/w key.png")
+w_key_after=pygame.image.load("final-project-team-hagil/assets/w key(after).png")
 while RUNNING:
-    w_key=pygame.image.load("final-project-team-hagil/assets/w key.png")
     screen.blit(w_key,((HEIGHT-1000),WIDTH-100))
     pygame.display.update()
     for event in pygame.event.get():
@@ -20,10 +21,15 @@ while RUNNING:
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_w:
                 print("Up")
-                screen.fill("blue")
+                screen.blit(w_key_after,((HEIGHT-1000),WIDTH-100))
+                pygame.time.wait(1000)
+                pygame.display.update()
+                screen.blit(w_key,((HEIGHT-1000),WIDTH-100))
+                pygame.time.wait(1)
                 pygame.display.flip()
-                pygame.time.wait(50)
-            elif event.key==pygame.K_s:
+            else:
+                screen.blit(w_key_after,((HEIGHT-1000),WIDTH-100))
+            if event.key==pygame.K_s:
                 print("Down")
                 screen.fill("red")
                 pygame.display.flip()
