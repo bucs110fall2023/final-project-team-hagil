9622182bfc3526a86783591619e5a5143a954fc1
Harry Do <qdo@binghamton.edu>
2023-11-16 21:07:12 -0500

    Change keyboard pic/ Succeed doing blinking button

--- src/controller.py
+++ src/controller.py
@@ -6,42 +6,41 @@ HEIGHT=1080
 screen_size=(HEIGHT,WIDTH)
 screen=pygame.display.set_mode(screen_size)
 RUNNING=True
-
+pressed=True
 screen.fill("white")
 pygame.display.flip()
 pygame.time.wait(1000)
 w_key=pygame.image.load("final-project-team-hagil/assets/w key.png")
 w_key_after=pygame.image.load("final-project-team-hagil/assets/w key(after).png")
+screen.blit(w_key,((HEIGHT-1000),WIDTH-100))
+pygame.display.update()
 while RUNNING:
-    screen.blit(w_key,((HEIGHT-1000),WIDTH-100))
-    pygame.display.update()
     for event in pygame.event.get():
-        if event.type ==pygame.QUIT:
-            RUNNING=False
-        elif event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_w:
-                print("Up")
-                screen.blit(w_key_after,((HEIGHT-1000),WIDTH-100))
-                pygame.time.wait(1000)
-                pygame.display.update()
-                screen.blit(w_key,((HEIGHT-1000),WIDTH-100))
-                pygame.time.wait(1)
-                pygame.display.flip()
-            else:
-                screen.blit(w_key_after,((HEIGHT-1000),WIDTH-100))
-            if event.key==pygame.K_s:
-                print("Down")
-                screen.fill("red")
-                pygame.display.flip()
-                pygame.time.wait(50)
-            elif event.key==pygame.K_a:
-                print("Left")
-                screen.fill("yellow")
-                pygame.display.flip()
-                pygame.time.wait(50)
-            elif event.key==pygame.K_d:
-                print("Right")
-                screen.fill("green")
-                pygame.display.flip()
-                pygame.time.wait(50)
-                
+            if event.type ==pygame.QUIT:
+                RUNNING=False
+            elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_w:
+                    print("Up")
+                    screen.blit(w_key_after,((HEIGHT-1000),WIDTH-100))
+                    pygame.time.wait(50)
+                    pygame.display.update()
+                elif event.key==pygame.K_s:
+                    print("Down")
+                    screen.fill("red")
+                    pygame.display.update()
+                    pygame.time.wait(50)
+                elif event.key==pygame.K_a:
+                    print("Left")
+                    screen.fill("yellow")
+                    pygame.display.update()
+                    pygame.time.wait(50)
+                elif event.key==pygame.K_d:
+                    print("Right")
+                    screen.fill("green")
+                    pygame.display.update()
+                    pygame.time.wait(50)
+            elif event.type==pygame.KEYUP:
+                if event.key==pygame.K_w:
+                    print("Up released")
+                    screen.blit(w_key,((HEIGHT-1000),WIDTH-100))
+                    pygame.display.update()    
