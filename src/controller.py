197c0e7eeb5e2a465db0441c716c7eea7b4969d2
Harry Do <qdo@binghamton.edu>
2023-11-18 20:30:06 -0500

    Fixing the button error

--- src/controller.py
+++ src/controller.py
@@ -26,15 +26,22 @@ a_key_after = pygame.image.load("final-project-team-hagil/assets/a key(after).pn
 d_key = pygame.image.load("final-project-team-hagil/assets/d key.png")
 d_key_after = pygame.image.load("final-project-team-hagil/assets/d key(after).png")
 
-space_key = w_key = pygame.image.load("final-project-team-hagil/assets/space key.png")
+space_key = pygame.image.load("final-project-team-hagil/assets/space key.png")
 space_key_after = pygame.image.load("final-project-team-hagil/assets/space key(after).png")
 
 
 screen.blit(w_key, ((SCREEN_HEIGHT-1000), (SCREEN_WIDTH-100)))
-screen.blit(space_key, ((SCREEN_HEIGHT-500), (SCREEN_WIDTH-100)))
+
+screen.blit(a_key, ((SCREEN_HEIGHT-1050), (SCREEN_WIDTH-55)))
+
+screen.blit(s_key, ((SCREEN_HEIGHT-1000), (SCREEN_WIDTH-55)))
+
+screen.blit(d_key, ((SCREEN_HEIGHT-950), (SCREEN_WIDTH-55)))
+
+screen.blit(space_key, ((SCREEN_HEIGHT-200), (SCREEN_WIDTH-100)))
 
 pygame.display.update()
-
+pygame.time.wait(10)
 
 RUNNING = True
 PRESSED = True
@@ -48,45 +55,38 @@ while RUNNING:
             elif event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_w:
                     print("Up")
-                    screen.blit(w_key_after,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
+                    screen.blit(w_key_after,((SCREEN_HEIGHT-1000), (SCREEN_WIDTH-100)))
                 elif event.key==pygame.K_s:
                     print("Down")
-                    screen.blit(s_key_after,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
-                    screen.fill("red")
+                    screen.blit(s_key_after,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-55))
                 elif event.key==pygame.K_a:
                     print("Left")
-                    screen.blit(a_key_after,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
-                    screen.fill("yellow")
+                    screen.blit(a_key_after,((SCREEN_HEIGHT-1050), (SCREEN_WIDTH-55)))
                 elif event.key==pygame.K_d:
                     print("Right")
-                    screen.blit(d_key_after,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
-                    screen.fill("green")
+                    screen.blit(d_key_after,((SCREEN_HEIGHT-950), (SCREEN_WIDTH-55)))
                 elif event.key==pygame.K_SPACE:
                     print("Space")
-                    screen.blit(space_key_after,(((SCREEN_HEIGHT-500), SCREEN_WIDTH-100)))
+                    screen.blit(space_key_after,(((SCREEN_HEIGHT-200), SCREEN_WIDTH-100)))
 
             elif event.type==pygame.KEYUP:
 
                 if event.key==pygame.K_w:
                     print("Up released")
                     screen.blit(w_key,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
-                    pygame.display.update()
                 elif event.key==pygame.K_s:
-                    print("Up released")
-                    screen.blit(s_key,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
-                    pygame.display.update()
+                    print("Down")
+                    screen.blit(s_key,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-55))
                 elif event.key==pygame.K_a:
-                    print("Up released")
-                    screen.blit(a_key,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
-                    pygame.display.update()
+                    print("Left")
+                    screen.blit(a_key,((SCREEN_HEIGHT-1050), (SCREEN_WIDTH-55)))
                 elif event.key==pygame.K_d:
-                    print("Up released")
-                    screen.blit(d_key,((SCREEN_HEIGHT-1000), SCREEN_WIDTH-100))
-                    pygame.display.update()  
+                    print("Right")
+                    screen.blit(d_key,((SCREEN_HEIGHT-950), (SCREEN_WIDTH-55)))
                 elif event.key==pygame.K_SPACE:
-                    screen.blit(space_key,(((SCREEN_HEIGHT-500), SCREEN_WIDTH-100)))
-
-            pygame.time.wait(50)
+                    print("Space")
+                    screen.blit(space_key,(((SCREEN_HEIGHT-200), SCREEN_WIDTH-100)))
+            pygame.time.wait(30)
             pygame.display.update()
 
 #hello
