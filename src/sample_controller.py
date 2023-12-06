4dfa1d76947c7efd1be4cd7aeea56c6be12f1f37
Harry Do <qdo@binghamton.edu>
2023-12-06 02:31:08 -0500

    Add new feature into the controller

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -3,7 +3,12 @@ import random
 from taskbar import Taskbar
 from obstacle import Obstacle
 from group import Group
-
+from mutagen.wave import WAVE
+import time
+def song_length(song_path):
+    audio =WAVE(song_path)
+    length=audio.info.length
+    return length         
 def choose_random(list_name):
     selection=[]
     chosen_value=()
@@ -32,7 +37,8 @@ def evaluate_ydistance(obstacle_sequence):
     elif obstacle_sequence==3 or obstacle_sequence==4 or obstacle_sequence==5:
         y=110
     return y
-
+def evaluate_time(start_time,current_time):
+    return current_time-start_time
 
 class Controller:
   
@@ -43,14 +49,21 @@ class Controller:
     self.taskbar_group=Group()# image size should be < 686x52px 
     
     
+    self.aim=400
+    self.main_aim=self.aim
+    self.aim2=0
     RUNNING=True
     
     #thread.start()
     self.total_win=[]
     self.total_lose=[]
     self.state="gameloop"
-    self.newkey=["w_key","s_key","a_key","d_key"]
-            
+    self.newkey=["w_key","s_key","a_key","d_key"]    
+    self.SONG={
+    "Careless":"final-project-team-hagil/assets/musics/careless.wav",
+    "Cross me":"final-project-team-hagil/assets/musics/careless.wav",
+    "I dont't care":"final-project-team-hagil/assets/musics/careless.wav"
+    }
     self.OG_KEY={
     "w_key":"final-project-team-hagil/assets/keyboard/w key.png",
     "s_key":"final-project-team-hagil/assets/keyboard/s key.png",
@@ -105,7 +118,7 @@ class Controller:
         pygame.init()
         self.screen=pygame.display.set_mode([self.SCREEN_HEIGHT,self.SCREEN_WIDTH])
         self.screen.fill("white")
-
+        
         pygame.display.flip()
         pygame.time.wait(60)
         self.gameloop()
@@ -122,21 +135,42 @@ class Controller:
       #redraw
       
   def gameloop(self):
+    
+    self.font=pygame.font.Font("final-project-team-hagil/assets/Caveat.ttf",49)    
+    self.music = pygame.mixer.music.load("final-project-team-hagil/assets/musics/careless.wav")
+    pygame.mixer.music.set_volume(0.2)
     self.random_value=choose_random(self.BG)
+    self.background=pygame.image.load(self.BG[self.random_value])
+    self.screen.blit(self.background,(0,0))
     self.taskbar=Taskbar(self.TASKBAR[self.random_value],540,665)
-    self.taskbar_group.add(self.taskbar)     
+    self.taskbar_group.add(self.taskbar) 
+
+    self.start_time=time.time()
     
+    self.song_time=song_length("final-project-team-hagil/assets/musics/careless.wav")
+        
+    pygame.mixer.music.play(-1)
     while self.RUNNING:
+        self.current_time=time.time()
+        self.elapsed_time=evaluate_time(self.start_time,self.current_time)
         self.OG_key()
-    
+        if self.elapsed_time>self.song_time:
+            self.RUNNING=False
         self.right_ans=0
         self.wrong_ans=0
-        pygame.display.update()
-    
+        pygame.display.update()             
+        self.msg2=str(self.aim2) + " misses"
+        self.msg= str(int(self.aim))+" combos left"
+        self.text2=self.font.render(self.msg2,True,"red")
+        self.text=self.font.render(self.msg,True,"white")
+        self.screen.blit(self.background,(0,0))
+        self.screen.blit(self.text,(0,0))
+        self.screen.blit(self.text2,(0,50))
+        self.OG_key()
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.RUNNING = False
-                    
+       
             
         self.taskbar_group.draw(self.screen)
         
@@ -167,6 +201,7 @@ class Controller:
             self.obstacle_group.draw(self.screen)
         self.obstacle_group.empty()    
         pygame.display.update()
+        self.create_time=time.time() 
         
         self.Testing=True
     
@@ -209,29 +244,44 @@ class Controller:
                         self.z+=1
                         self.answer.pop(0)
                         self.obstacle_group.empty()
-                    elif self.key[-1]!=self.answer[0]:        
-                        self.obstacle.update(self.FALSE_KEY[self.answer[0]],self.coordinate[self.z],self.y_coord)
-                        self.wrong_ans+=1
-                        self.obstacle_group.add(self.obstacle)
-                        self.obstacle_group.draw(self.screen)
-                        self.z+=1
-                        self.answer.pop(0)
-                        self.obstacle_group.empty()
+                    elif self.key[-1]!=self.answer[0]:   
+                        if self.key[-1]=="submit":
+                            self.Testing=False     
+                        else:
+                            self.obstacle.update(self.FALSE_KEY[self.answer[0]],self.coordinate[self.z],self.y_coord)
+                            self.wrong_ans+=1
+                            self.obstacle_group.add(self.obstacle)
+                            self.obstacle_group.draw(self.screen)
+                            self.z+=1
+                            self.answer.pop(0)
+                            self.obstacle_group.empty()
                     pygame.display.flip()
                     if self.key[-1]=="submit":
-                        self.total_win.append(self.right_ans)
-                        self.total_lose.append(self.wrong_ans)
+                        self.aim=self.aim-self.right_ans
+                        self.aim2=self.aim2+self.wrong_ans
+                        if self.aim2>10:
+                            self.RUNNING=False
                         self.Testing=False
                     elif  self.answer==[]:
-                        pygame.time.wait(20) 
-                        self.total_win.append(self.right_ans)
-                        self.total_lose.append(self.wrong_ans)
-                        self.key=[]  
-                        self.Testing=False                    
-                  
-                    
-                    
-
+                        self.aim=self.aim-self.right_ans
+                        self.aim2=self.aim2+self.wrong_ans
+                        if self.aim2>10:
+                            self.RUNNING=False
+                        pygame.time.wait(20)
+                        self.Testing=False    
+            self.solving_time=time.time()                
+            if evaluate_time(self.create_time,self.solving_time)>3:
+                self.aim=self.aim-self.right_ans
+                self.aim2=self.aim2+self.wrong_ans
+                if self.aim2>10:
+                    self.RUNNING=False
+                    pygame.time.wait(20) 
+                self.Testing=False
+                self.obstacle_group.empty()
+            self.current_time=time.time()
+            elapsed_time=evaluate_time(self.start_time,self.current_time)
+            if elapsed_time>self.song_time:
+                self.RUNNING=False
                 pygame.display.flip()
             pygame.display.flip()
     print(self.total_lose,self.total_win)     
