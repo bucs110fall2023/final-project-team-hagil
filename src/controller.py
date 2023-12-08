7cc296d6042df907fe3a83e1a57c994edd0a0ab4
Gilbert Kim <gilbertkim04@gmail.com>
2023-12-08 15:59:16 -0500

    End

--- src/controller.py
+++ src/controller.py
@@ -1,24 +1,29 @@
 import pygame
 import random
+import time
+
+from src.button import Button
 from src.taskbar import Taskbar
 from src.obstacle import Obstacle
 from src.group import Group
-from src.button import Button
 from mutagen.wave import WAVE
-import time
-def song_length(song_path):
-    audio =WAVE(song_path)
-    length=audio.info.length
+
+
+def song_length(song_path): 
+    """get the length of the song, arguement is getting the information of the file then return the length of the song in seconds"""
+    audio = WAVE(song_path)
+    length = audio.info.length
     return length         
 def choose_random(list_name):
+    """choose a random value from a dictionary"""
     selection=[]
     chosen_value=()
     for k in list_name :
         selection.append(k)
     chosen_value=random.choice(selection)
     return chosen_value
-
 def evaluate_xdistance(obstacle_sequence):
+    """calculate the obstacle placement base on the number of obstacles"""
     if obstacle_sequence==10 or obstacle_sequence==6:
         x=300
     elif obstacle_sequence==7 or obstacle_sequence==8 or obstacle_sequence==9:
@@ -26,8 +31,8 @@ def evaluate_xdistance(obstacle_sequence):
     elif obstacle_sequence==3 or obstacle_sequence==4 or obstacle_sequence==5:
         x=400-(obstacle_sequence-3)*50
     return x
-
 def evaluate_ydistance(obstacle_sequence):
+    """calculate the distance between obstacles base on the number of obstacles"""
     if obstacle_sequence==10 or obstacle_sequence==6:
         if obstacle_sequence==10:
             y=50
@@ -39,21 +44,21 @@ def evaluate_ydistance(obstacle_sequence):
         y=110
     return y
 def evaluate_time(start_time,current_time):
+    """calculate the time elapsed"""
     return current_time-start_time
 
-class Controller:
-  
+
+class Controller: 
   def __init__(self):
+    """This contains the variables and dictionary that will be used in the game"""
     
     self.obstacle_group=Group()
     self.state=()
-    self.taskbar_group=Group()# image size should be < 686x52px 
-    self.Ending=True
-    self.aim=0
-    self.main_aim=0
-    self.aim2=0
-    RUNNING=True
-    #thread.start()
+    self.taskbar_group=Group()
+    self.ending=True
+    self.correct_ans=0
+    self.base_score=0
+    self.incorrect_ans=0
     self.state="gameloop"
     self.newkey=["w_key","s_key","a_key","d_key"]    
     self.ICONS={
@@ -77,7 +82,7 @@ class Controller:
     "d_key":"assets/keyboard/d key.png",
     "space_key":"assets/keyboard/space key.png"
 }
-    self.Al_KEY={
+    self.AL_KEY={
     "w_key":"assets/keyboard/w key(after).png",
     "s_key":"assets/keyboard/s key(after).png",
     "a_key":"assets/keyboard/a key(after).png",
@@ -108,16 +113,29 @@ class Controller:
     self.SCREEN_WIDTH = 720
     self.SCREEN_HEIGHT = 1080
     self.SCREEN_SIZE = (self.SCREEN_HEIGHT, self.SCREEN_WIDTH)
-    self.RUNNING=True
-    self.Testing=True
+    self.running=True
+    self.testing=True
     self.working=True
   def OG_key(self):
+    """display the key on the screen"""
     self.screen.blit(pygame.image.load(self.OG_KEY["w_key"]),(self.SCREEN_HEIGHT-1000,self.SCREEN_WIDTH-100))
     self.screen.blit(pygame.image.load(self.OG_KEY["s_key"]),(self.SCREEN_HEIGHT-1000,self.SCREEN_WIDTH-55))
     self.screen.blit(pygame.image.load(self.OG_KEY["a_key"]),(self.SCREEN_HEIGHT-1050,self.SCREEN_WIDTH-55))
     self.screen.blit(pygame.image.load(self.OG_KEY["d_key"]),(self.SCREEN_HEIGHT-950,self.SCREEN_WIDTH-55))
     self.screen.blit(pygame.image.load(self.OG_KEY["space_key"]),(self.SCREEN_HEIGHT-150,self.SCREEN_WIDTH-100))
+  def number_of_combos(self, song_time):
+    """calculate the number of combos base on the length of the song"""
+    if song_time>100:
+            if song_time>=240:
+                self.correct_ans=450
+            elif song_time>=180:
+                self.correct_ans=350
+            elif song_time>=120:
+                self.correct_ans=250
+            else:
+                self.correct_ans=200
   def mainloop(self):
+        """This contains different stages of the game"""
         pygame.init()
         self.screen=pygame.display.set_mode([self.SCREEN_HEIGHT,self.SCREEN_WIDTH])
         self.start_img=pygame.image.load("assets/others icons/start.png").convert_alpha()
@@ -133,19 +151,8 @@ class Controller:
                 self.gameloop()
             elif self.state=="gameoverloop":
                 self.gameoverloop()
-    #select state loop
-  
-  ### below are some sample loop states ###
-
-  #def menuloop(self):
-    
-      #event loop
-
-      #update data
-
-      #redraw
-  
   def start_screen(self):
+    """display the start screen"""
     self.run = True
     while self.run:
         self.main_screen = pygame.image.load("assets/main_screen.jpg")
@@ -159,30 +166,21 @@ class Controller:
             elif event.type == pygame.QUIT:
                 self.run = False
                 self.working=False
-                
-    
   def gameloop(self):
-    self.aim=0
-    self.main_aim=0
-    self.aim2=0
+    """display the game mechanisms"""
+    self.correct_ans=0
+    self.base_score=0
+    self.incorrect_ans=0
     self.obstacle_group.empty()
-    self.RUNNING=True
-    self.Testing=True
+    self.running=True
+    self.testing=True
     self.start=True    
     self.font=pygame.font.Font("assets/Caveat.ttf",49) 
     self.random_song=choose_random(self.SONG)   
     self.music = pygame.mixer.music.load(self.SONG[self.random_song])
     self.song_time=song_length(self.SONG[self.random_song])
-    if self.song_time>100:
-        if self.song_time>=240:
-            self.aim=450
-        elif self.song_time>=180:
-            self.aim=350
-        elif self.song_time>=120:
-            self.aim=250
-        else:
-            self.aim=200
-    self.main_aim=self.aim
+    self.number_of_combos(self.song_time)
+    self.base_score=self.correct_ans
     pygame.mixer.music.set_volume(0.2)
     self.random_value=choose_random(self.BG)
     self.background=pygame.image.load(self.BG[self.random_value])
@@ -191,7 +189,7 @@ class Controller:
     self.taskbar_group.add(self.taskbar)
     for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.RUNNING = False
+                    self.running = False
                     self.working=False
     while self.start:
         for k in self.ICONS:
@@ -203,18 +201,18 @@ class Controller:
         self.start=False
     self.start_time=time.time()
     pygame.mixer.music.play(-1)
-    while self.RUNNING:
+    while self.running:
         self.current_time=time.time()
         self.elapsed_time=evaluate_time(self.start_time,self.current_time)
         self.OG_key()
         if self.elapsed_time>self.song_time-5:
-            self.RUNNING=False
+            self.running=False
             pygame.mixer_music.stop()
         self.right_ans=0
         self.wrong_ans=0
         pygame.display.update()             
-        self.msg2=str(self.aim2) + " misses"
-        self.msg= str(int(self.aim))+" combos left"
+        self.msg2=str(self.incorrect_ans) + " misses"
+        self.msg= str(int(self.correct_ans))+" combos left"
         self.text2=self.font.render(self.msg2,True,"red")
         self.text=self.font.render(self.msg,True,"white")
         self.screen.blit(self.background,(0,0))
@@ -248,38 +246,38 @@ class Controller:
         self.obstacle_group.empty()    
         pygame.display.update()
         self.create_time=time.time() 
-        self.Testing=True
+        self.testing=True
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 self.working=False
-        while self.Testing:
+        while self.testing:
             
             self.OG_key()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.Testing=False
-                    self.RUNNING = False
+                    self.testing=False
+                    self.running = False
                     self.working=False
                 elif event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_w:
-                        self.screen.blit(pygame.image.load(self.Al_KEY["w_key"]),(self.SCREEN_HEIGHT-1000,self.SCREEN_WIDTH-100))
+                        self.screen.blit(pygame.image.load(self.AL_KEY["w_key"]),(self.SCREEN_HEIGHT-1000,self.SCREEN_WIDTH-100))
                         self.key.append("w_key")
                     elif event.key==pygame.K_s:
                         self.key.append("s_key")
-                        self.screen.blit(pygame.image.load(self.Al_KEY["s_key"]),(self.SCREEN_HEIGHT-1000,self.SCREEN_WIDTH-55))
+                        self.screen.blit(pygame.image.load(self.AL_KEY["s_key"]),(self.SCREEN_HEIGHT-1000,self.SCREEN_WIDTH-55))
                     elif event.key==pygame.K_a:
-                        self.screen.blit(pygame.image.load(self.Al_KEY["a_key"]),(self.SCREEN_HEIGHT-1050,self.SCREEN_WIDTH-55))
+                        self.screen.blit(pygame.image.load(self.AL_KEY["a_key"]),(self.SCREEN_HEIGHT-1050,self.SCREEN_WIDTH-55))
                         self.key.append("a_key")
                     elif event.key==pygame.K_d:
-                        self.screen.blit(pygame.image.load(self.Al_KEY["d_key"]),(self.SCREEN_HEIGHT-950,self.SCREEN_WIDTH-55))
+                        self.screen.blit(pygame.image.load(self.AL_KEY["d_key"]),(self.SCREEN_HEIGHT-950,self.SCREEN_WIDTH-55))
                         self.key.append("d_key")             
                     elif event.key==pygame.K_SPACE:
                         self.key.append("submit")
-                        self.screen.blit(pygame.image.load(self.Al_KEY["space_key"]),(self.SCREEN_HEIGHT-150,self.SCREEN_WIDTH-100))
+                        self.screen.blit(pygame.image.load(self.AL_KEY["space_key"]),(self.SCREEN_HEIGHT-150,self.SCREEN_WIDTH-100))
                     pygame.display.flip()
                     self.obstacle_group.empty()
                     if self.key[-1]==self.answer[0]:        
-                        self.obstacle.update(self.Al_KEY[self.answer[0]],self.coordinate[self.z],self.y_coord)
+                        self.obstacle.update(self.AL_KEY[self.answer[0]],self.coordinate[self.z],self.y_coord)
                         self.right_ans+=1
                         self.obstacle_group.add(self.obstacle)
                         self.obstacle_group.draw(self.screen)
@@ -288,7 +286,7 @@ class Controller:
                         self.obstacle_group.empty()
                     elif self.key[-1]!=self.answer[0]:   
                         if self.key[-1]=="submit":
-                            self.Testing=False     
+                            self.testing=False     
                         elif self.key[-1]=="w_key" or self.key[-1]=="a_key" or self.key[-1]=="s_key" or self.key[-1]=="d_key":
                             self.obstacle.update(self.FALSE_KEY[self.answer[0]],self.coordinate[self.z],self.y_coord)
                             self.wrong_ans+=1
@@ -301,63 +299,65 @@ class Controller:
                             self.key.append("error")
                     pygame.display.flip()
                     if self.key[-1]=="submit":
-                        self.aim=self.aim-self.right_ans
-                        self.aim2=self.aim2+self.wrong_ans
-                        self.Testing=False
+                        self.correct_ans=self.correct_ans-self.right_ans
+                        self.incorrect_ans=self.incorrect_ans+self.wrong_ans
+                        self.testing=False
                     elif  self.answer==[]:
-                        self.aim=self.aim-self.right_ans
-                        self.aim2=self.aim2+self.wrong_ans
-                        self.Testing=False
-                    if self.aim2>20:
-                        self.RUNNING=False
+                        self.correct_ans=self.correct_ans-self.right_ans
+                        self.incorrect_ans=self.incorrect_ans+self.wrong_ans
+                        self.testing=False
+                    if self.incorrect_ans>20:
+                        self.running=False
                         pygame.mixer_music.stop()  
-                    elif self.aim<=0:
-                        self.aim=0
-                        self.aim2=0  
+                    elif self.correct_ans<=0:
+                        self.correct_ans=0
+                        self.incorrect_ans=0  
             self.solving_time=time.time()     
             self.current_time=time.time()
             elapsed_time=evaluate_time(self.start_time,self.current_time)           
-            if evaluate_time(self.create_time,self.solving_time)>2:
-                self.aim=self.aim-self.right_ans
-                self.aim2=self.aim2+self.wrong_ans
-                if self.aim2>20:
-                    self.RUNNINaG=False
-                    pygame.time.wait(20) 
-                self.Testing=False
+            if evaluate_time(self.create_time,self.solving_time)>3:
+                self.correct_ans=self.correct_ans-self.right_ans
+                self.incorrect_ans=self.incorrect_ans+self.wrong_ans
+                if self.incorrect_ans>20:
+                    self.running=False
+                    pygame.time.wait(20)
+                    pygame.mixer_music.stop
+                self.testing=False
                 self.obstacle_group.empty()
             elif elapsed_time>self.song_time-5:
-                self.RUNNING=False
+                self.running=False
                 pygame.mixer_music.stop()
                 pygame.display.flip()
             pygame.display.flip()     
     self.state="gameoverloop"
   def gameoverloop(self):
-        print(self.main_aim-self.aim)
-        self.Ending=True
+        """display the message and score after the game is over"""
+        self.ending=True
         self.home=Obstacle("assets/home.png",0,0)
         self.reset=Obstacle("assets/reset.png",980,0)
         self.obstacle_group.empty()
         self.font=pygame.font.Font("assets/San serif.otf",45)
-        self.end_msg= "Target: "+ str(self.main_aim)
-        self.end_msg2= "Finised combos: " + str(self.main_aim-self.aim)
-        self.end_msg3=" Misses combo: " + str(self.aim2)
+        self.end_msg= "Target: "+ str(self.base_score)
+        self.end_msg2= "Finised combos: " + str(self.base_score-self.correct_ans)
         self.text=self.font.render(self.end_msg,True,"cadetblue")
         self.text2=self.font.render(self.end_msg2, True, "cadetblue")
+        self.end_msg3=" Misses combo: " + str(self.incorrect_ans)
         self.text3=self.font.render(self.end_msg3, True, "cadetblue")
-        if self.aim<=0 and self.aim2<20:
+        if self.correct_ans<=0 and self.incorrect_ans<20:
             self.announcement=pygame.image.load("assets/you win.png")
-        elif self.aim2>=20 or self.aim >0:
+        elif self.incorrect_ans>=20 or self.correct_ans >0:
             self.announcement=pygame.image.load("assets/you lose.png")
         else:
             self.announcement=pygame.image.load("assets/you lose.png")
-        while self.Ending:
+        while self.ending:
             self.screen.fill("plum1")
             self.screen.blit(pygame.image.load("assets/game_over (1).png"),(self.SCREEN_HEIGHT-750,0))
             self.home_rect=pygame.Rect(0,0,100,90)
             self.reset_rect=pygame.Rect(980,0,100,100)
             self.screen.blit(self.text,(self.SCREEN_HEIGHT-670,self.SCREEN_WIDTH/2-150))
             self.screen.blit(self.text2,(self.SCREEN_HEIGHT-740,self.SCREEN_WIDTH/2-100))
-            self.screen.blit(self.text3,(self.SCREEN_HEIGHT-720,self.SCREEN_WIDTH/2-50))
+            if self.incorrect_ans>20 or self.correct_ans>0:
+                self.screen.blit(self.text3,(self.SCREEN_HEIGHT-720,self.SCREEN_WIDTH/2-50))
             self.screen.blit(self.announcement,(self.SCREEN_HEIGHT-850,self.SCREEN_WIDTH/2))
             self.obstacle_group.add(self.home)
             self.obstacle_group.draw(self.screen)
@@ -367,16 +367,18 @@ class Controller:
             pygame.time.wait(300)
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
-                    self.Ending=False
+                    self.ending=False
                     self.working=False
                 elif event.type == pygame.MOUSEBUTTONDOWN: 
                     if self.home_rect.collidepoint(event.pos):
                         if event.button==1:
-                            self.Ending=False
+                            self.ending=False
                             self.state="mainscreen"
                     elif self.reset_rect.collidepoint(event.pos):
                         if event.button==1:    
-                            self.Ending=False
+                            self.ending=False
                             self.state="gameloop"
+
+
 control=Controller()
 control.mainloop()
